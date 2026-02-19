1a42af665416b32b142ea2fb773ee2ff1c8119bf
Clement PRAUD <clement.praud2025@campus-eni.fr>
2026-02-19 14:39:31 +0100

    create search page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Register } from './pages/register/register';
 import { Login } from './pages/login/login';
+import { Search } from './pages/search/search';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
     {
@@ -12,6 +14,9 @@ export const routes: Routes = [
     },
     {
         path: "login", component: Login
+    },
+    {
+        path: "search", component: Search, canActivate: [authGuard]
     },
     {
         path: "**", redirectTo: "", pathMatch: "full"
